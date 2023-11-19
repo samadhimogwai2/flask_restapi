8794f51808267d50f869ec83188924700fd76afd
Jona <87903362+samadhimogwai2@users.noreply.github.com>
2023-11-19 13:29:00 +0900

    5th

--- 2_data/app3/test.py
+++ 2_data/app3/test.py
@@ -12,6 +12,7 @@ def main():
     
     # ローカルホスト（http://127.0.0.1:5000）上で実行されているRESTful APIに対して
     # QUERY_DATAをパラメータとして受け渡し、GETリクエストを送信
+    # URLはhttp://127.0.0.1:5000/?username=Tatsukiになる
     response = requests.get(URL, params=QUERY_DATA)
     print(f"\nurl: {response.url}")
     print(f"status code: {response.status_code}")
